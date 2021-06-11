9583a23a1d67a3de2a60dc2faaf21ba7f5675cb2
18wh1a1207 <53571884+18wh1a1207@users.noreply.github.com>
2021-06-11 14:32:40 +0530

    Create exp 2.php

---
+++ CN&WTLab/WT Experiments/Experiment 2/exp 2.php
@@ -0,0 +1,21 @@
+1.	
+<?php 
+    $str = "Chandana"; 
+    echo strlen($str); 
+?>  
+2.
+<?php
+echo str_word_count("Chandana from 3rd IT");
+?>
+
+3.
+<?php
+echo strrev("Chandana from 3rd IT");
+?>
+
+4.
+<?php
+if (strpos('A b c d', 'b')) {
+    echo "Checking the existence of the empty string will always return true";
+}
+?>
