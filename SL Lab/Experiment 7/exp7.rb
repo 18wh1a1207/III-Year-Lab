e4772f96a2d67c64e3ae3a98a0de87c62d224c26
18wh1a1207 <53571884+18wh1a1207@users.noreply.github.com>
2021-07-14 20:01:14 +0530

    Create exp7.rb

---
+++ SL Lab/Experiment 7/exp7.rb
@@ -0,0 +1,11 @@
+puts "Enter First Number "
+a = gets.chomp.to_i
+puts "Enter Second Number "
+b = gets.chomp.to_i
+
+if a == 20 || b == 20
+    puts "True"
+else
+    puts "sum of two numbers is", a + b
+    
+end
